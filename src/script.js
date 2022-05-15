6d8392cd826140f8953a74090d0252481a443521
Jesus Cabrita <jesusarnaldo115@gmail.com>
2022-05-15 17:30:21 -0300

    Darkfire

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function hello(person) {
+    return "Hello, " + person;
+}
+var user = "Jesus Arnaldo Cabrita Rodriguez";
+var result = hello(user);
+console.log(result);
---
+++ src/script.ts
@@ -0,0 +1,6 @@
+function hello(person) {
+  return "Hello, " + person;
+}
+let user = "Jesus Arnaldo Cabrita Rodriguez";
+const result = hello(user);
+console.log(result);
